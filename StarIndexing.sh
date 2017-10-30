ef296a0469299cab77fc43315cf05c8b8a4dd594
niyaznihmath <niyaznihmath@users.noreply.github.com>
2017-10-31 00:02:51 +0100

    Update StarIndexing.sh

--- StarIndexing.sh
+++ StarIndexing.sh
@@ -12,3 +12,7 @@ module load bioinfo-tools
 module load star/2.5.3a
 
 STAR --runMode genomeGenerate --runThreadN 4 --genomeDir /proj/b2014097/nobackup/Niyaz/STAR/STAR-Index/ --genomeFastaFiles /proj/b2014097/nobackup/Niyaz/STAR/GRCh38.p10.genome.fa --sjdbGTFfile /proj/b2014097/nobackup/Niyaz/STAR/gencode.v27.annotation.gtf --sjdbOverhang 99
+
+## --sjdbOverhang (specifies the length of the genomic sequence around the annotated junction to be used in constructing the splice junctions database. 
+## Ideally, this length should be equal to the ReadLength-1, where ReadLength is the length of the reads. For instance, for Illumina 2x100b paired-end reads, the ideal value is 100-1=99. 
+## In case of reads of varying length, the ideal value is max(ReadLength)-1. In most cases, a generic value of 100 will work as well as the ideal value.)
